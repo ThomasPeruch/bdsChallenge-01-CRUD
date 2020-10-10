cfd6caf8c494644d1d7dd87c9308464f9d61be9c
ThomasPeruch <peruchthomas@gmail.com>
2020-10-10 15:33:08 -0300

    DTO

--- src/main/java/com/tproject/crudchallenge/resources/ClientResource.java
+++ src/main/java/com/tproject/crudchallenge/resources/ClientResource.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.tproject.crudchallenge.dto.ClientDTO;
 import com.tproject.crudchallenge.entities.Client;
 import com.tproject.crudchallenge.services.ClientService;
 
@@ -23,9 +25,15 @@ public class ClientResource {
 	private ClientService service;
 		
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id); 
+		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PostMapping
--- src/main/java/com/tproject/crudchallenge/services/ClientService.java
+++ src/main/java/com/tproject/crudchallenge/services/ClientService.java
@@ -1,10 +1,14 @@
 package com.tproject.crudchallenge.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tproject.crudchallenge.dto.ClientDTO;
 import com.tproject.crudchallenge.entities.Client;
 import com.tproject.crudchallenge.repositories.ClientRepository;
 
@@ -14,12 +18,24 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
-	public List<Client> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly=true)
+	public List<ClientDTO> findAll(){
+		List<Client> list= repository.findAll();
+		return list.stream().map(cli -> new ClientDTO(cli)).collect(Collectors.toList());			
+	}
+	
+	@Transactional(readOnly=true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> opt = repository.findById(id);
+		Client entity = opt.get();	
+		return new ClientDTO(entity);
+		
 	}
 
 	public Client create(Client client) {
 		client = repository.save(client); 
 		return client;
 	}
+	
+	
 }
