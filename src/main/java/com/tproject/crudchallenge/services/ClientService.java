060b64f2d7058ea620475ed7718c7ec0c791258f
ThomasPeruch <peruchthomas@gmail.com>
2020-10-11 21:09:57 -0300

    update(PUT) and delete(DELETE) methods

--- src/main/java/com/tproject/crudchallenge/services/ClientService.java
+++ src/main/java/com/tproject/crudchallenge/services/ClientService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,7 +31,7 @@ public class ClientService {
 	@Transactional(readOnly=true)
 	public ClientDTO retrieveById(Long id) {
 		Optional<Client> opt = repository.findById(id);
-		Client entity = opt.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));	
+		Client entity = opt.orElseThrow(() -> new ResourceNotFoundException("id " + id + " not found"));	
 		return new ClientDTO(entity);
 		
 	}
@@ -36,13 +39,43 @@ public class ClientService {
 	@Transactional
 	public ClientDTO create(ClientDTO dto) {
 		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity); 
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id " + id + " not found");
+		}
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("id " + id + " not found");
+		}
+	
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto,Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
-		entity = repository.save(entity); 
-		return new ClientDTO(entity);
 	}
+
+	
 	
 }
